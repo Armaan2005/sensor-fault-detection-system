7766623a7f68ded5ec945677597e888f4459d98c
Armaan2005 <vishu18012005@gmail.com>
2025-11-22 19:43:22 +0530

    Added modules

---
+++ src/constant/__init__.py
@@ -0,0 +1,12 @@
+import os
+
+AWS_S3_BUCKET_NAME="water-fault"
+MONGO_DATABASE_NAME="pwskills"
+MONGO_COLLECTION_NAME="waterfault"
+
+TARGET_COLUMN="quality"
+MONGO_DB_URL="mongodb+srv://snshrivas:Snshrivas@cluster0.u46c4.mongodb.net/?retryWrites=true&w=majority"
+
+
+MODEL_FILE_NAME="model"
+MODEL_FILE_EXTENSION=".pkl"
